fd987b0b9ce7bf92e6560e5163fa4c55f1be8a87
ChepkemoiMaurene <baurene.chepkemoi@student.moringaschool.com>
2022-10-05 12:33:09 +0300

    added GET data function to obtain data from our local server

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,45 @@
 
 import './App.css';
+import Navbar from './Navbar';
+import {Switch, Route} from "react-router-dom"
+import ContactUs from "./ContactUs"
+import AboutUs from "./AboutUs"
+import Home from "./Home"
+import BookAppointment from "./BookAppointment"
+import { useEffect, useState } from 'react';
 
 function App() {
+  const[data ,setData]=useState([])
+  useEffect(()=>{
+    fetch('http://localhost:3000/workout')
+    .then(response=>response.json())
+    .then(data=>
+      {
+        setData(data)
+        console.log(data)
+      
+      })
+    },[])
+
   return (
     <div>
-      <h1>App components</h1>
+     <Navbar/>
+     <hr></hr>
+     <Switch>
+      <Route exact path='/about'>
+        <AboutUs/>
+      </Route>
+      <Route exact path="/contactus">
+        <ContactUs/>
+      </Route>
+      <Route  exact path="bookappointment">
+        <BookAppointment/>
+      </Route>
+      <Route exact path="/">
+        <Home/>
+      </Route>
+
+     </Switch>
     </div>
   );
 }
