fefc7c43a051cf47ea4b87aa42d3dbe2608f4c80
ChepkemoiMaurene <baurene.chepkemoi@student.moringaschool.com>
2022-10-06 12:04:22 +0300

    added icons to contactUs

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,15 +1,25 @@
-// import React from 'react'
+import React from 'react'
 
-// function ContactUs({data}) {
-//   const  findData=data.map((everyData)=>(
-//     <>
+function ContactUs({data}) {
+  const  findData=data.map((everyData)=>(
+    <>
+<div>
+     <img src='https://media.istockphoto.com/vectors/fitness-vector-abstract-design-templatedesign-for-gym-and-fitness-vector-id1144084265?k=20&m=1144084265&s=612x612&w=0&h=t4m6NE8NRCQIOLSFMmtj1qlfJJPao-t8GQ8jVPEulLw=' alt='logo'></img>
+     <h1>Tsunami website</h1>
+     <img src="https://fontawesome.com/icons/facebook" alt='facebook'></img>
+     <img src="https://fontawesome.com/icons/twitter" alt="twitter"></img>
+     <img src="https://fontawesome.com/icons/instagram"alt="insta"></img>
+     <p></p>
 
+</div>
 
-//     </>
-//   ))
-//   return (
-//     <div>ContactUs</div>
-//   )
-// }
+    </>
+  ))
+  return (
+    <div>ContactUs
+    {findData}
+    </div>
+  )
+}
 
-// export default ContactUs
+export default ContactUs
