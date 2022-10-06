364a1656b6e350f9bc72bfdca7463442326a709f
ChepkemoiMaurene <baurene.chepkemoi@student.moringaschool.com>
2022-10-06 10:58:26 +0300

    added Classes to inputs

--- src/components/BookAppointment.js
+++ src/components/BookAppointment.js
@@ -1,8 +1,18 @@
+
 import React from 'react'
 
 function BookAppointment() {
   return (
-    <div>BookAppointment</div>
+    <div >
+<form className='form'>
+  <label> fill in the form</label>
+  <input className='input' type="email" placeholder='email...'/>
+  <input className='input' type="date" placeholder='enter date...'/>
+  <input className='input' type="contact" placeholder='eneter phone number'/>
+  <input className='input' type="tel"placeholder="+254..."/>
+  <input type="submit" value='Book now'/>
+</form>
+      </div>
   )
 }
 
