486fa05a49c722df16c91769021499e005930e74
ChepkemoiMaurene <baurene.chepkemoi@student.moringaschool.com>
2022-10-07 18:59:45 +0300

    added a header tag

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -4,12 +4,30 @@ function AboutUs() {
  
   return (
     <div> 
-<h1> welcome to tsunami gym</h1>
+<h1> WELCOME TO TSUNAMI GYM</h1>
 <p>Get your desired body  using our qualified trainers and healthy meal plans offered to you by our able trainers</p>
   
+  <h1 style={{color:'orangered'}}>OUR STORY</h1>
+<p>Gym and Fitness was founded in 2002 as a family owned and <br/>
+operated business specialising in supplying
+   high-quality gym equipment at great prices.<br/>
+
+Instead of being just another gym equipment retailer, our founders wanted to be the best in the
+ industry and set their minds to doing so! Over
+ the last two decades Gym and Fitness has grown into one of Kenya’s largest online fitness 
+ equipment retailers, helping thousands of customers live longer, happier and healthier lives.</p>
   
-  
-  
+  <h1 style={{color:'orangered'}}>OUR PEOPLE</h1>
+<p>Gym and Fitness has a growing team of 60+ awesome superstars <br/>
+and we owe our success to each and every one of them!
+
+We have some pretty amazing people working with us, who hustle everyday <br/>
+to give you the best possible experience! You can find them in areas such as sales, <br/>
+marketing and e-commerce, customer service, warehousing, finance, human resources and management.
+
+If you're looking for an opportunity to grow at a company <br/>
+that's empowered, inclusive, and values great work—you should work with us.</p>
+
   
 </div>
   )
