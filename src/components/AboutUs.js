99f3d26607fa35e1d8d88633475ddc19b0994534
ChepkemoiMaurene <baurene.chepkemoi@student.moringaschool.com>
2022-10-07 18:44:33 +0300

    added information to aboutUs

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,8 +1,17 @@
 import React from 'react'
 
 function AboutUs() {
+ 
   return (
-    <div>AboutUs</div>
+    <div> 
+<h1> welcome to tsunami gym</h1>
+<p>Get your desired body  using our qualified trainers and healthy meal plans offered to you by our able trainers</p>
+  
+  
+  
+  
+  
+</div>
   )
 }
 
