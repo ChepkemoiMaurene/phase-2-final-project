59603c8abb42a4432f5950b6807568b7035324f6
ChepkemoiMaurene <baurene.chepkemoi@student.moringaschool.com>
2022-10-07 18:43:12 +0300

    added listeners and functions

--- src/components/BookAppointment.js
+++ src/components/BookAppointment.js
@@ -1,18 +1,46 @@
 
-import React from 'react'
+import React,{useState} from 'react'
 
 function BookAppointment() {
+  const[contactUs, setContactUs]=useState({
+    email:"",
+    date:"",
+    fullname:"",
+    tel:""
+  })
+  function handleChange(e){
+      let name=e.target.name
+      let value=e.target.value
+      setContactUs({
+        ...contactUs, [name]:value,
+        
+      })
+  }
+ 
+  function handleSubmit(e){
+      e.preventDefault();
+      fetch('http://localhost:3000/book',{
+        method:"POST",
+        headers:{
+          "Content-Type":"application/json"
+        },
+        body:JSON.stringify(contactUs)
+
+      })
+      .then(res=>res.json())
+      .then(data=>console.log(data))
+  }
   return (
     <div >
-<form className='form'>
-  <label> fill in the form</label>
-  <input className='input' type="email" placeholder='email...'/>
-  <input className='input' type="date" placeholder='enter date...'/>
-  <input className='input' type="contact" placeholder='eneter phone number'/>
-  <input className='input' type="tel"placeholder="+254..."/>
-  <input type="submit" value='Book now'/>
-</form>
-      </div>
+      <form onSubmit={handleSubmit} className='form'>
+        <label> fill in the form</label>
+        <input onChange={handleChange} className='input' type="email" placeholder='email...' name='email'/>
+        <input onChange={handleChange} className='input' type="date" placeholder='enter date...' name='date'/>
+        <input onChange={handleChange} className='input' type="name" placeholder='enter your fullname...'name='fullname'/>
+        <input onChange={handleChange} className='input' type="tel"placeholder="+254..." name='tel'/>
+        <input  type="submit"value='Book now'/>
+      </form>
+    </div>
   )
 }
 
